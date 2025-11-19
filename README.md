543ebc05e0daf7f6eb64f4dba47463f5caae00a9
07martin12 <alexmartin9c@gmail.com>
2025-11-19 19:12:57 -0300

    estructura de carpetas reorganizada

--- README.md
+++
@@ -1,4 +0,0 @@
-//archivos .json para abm
-//archivos .json para user
-//arreglar alineacion de botones del user
-// achicar foto del user para mobile
---
+++ src/ts/user.js
@@ -0,0 +1,30 @@
+"use strict";
+/*
+  cargar selects desde ts para pais y eliminar cuenta
+
+<p id="usernameDisplay"></p>
+  <div class="foto_perfil_wrapper">
+          <img
+            src="../../assets/img/icons/fotoPerfilDefault.png"
+            alt="Foto de perfil"
+            class="foto_perfil"
+          />
+           <img
+            src="../../assets/img/icons/fotoPerfilDefault.png"
+            alt="Foto de perfil"
+            class="foto_perfil"
+          />
+        </div>
+se cargan desde ts por defecto
+
+se analiza usuarios.json para cargar esos datos por defecto
+
+ademas se toman los datos de ese usuario y se van publicando dentro
+del input correspondiente como placeholders
+
+spa para los 3 botones especiales
+
+añadir boton configuracion?
+
+
+*/
